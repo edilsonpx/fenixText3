c6205200bdecd2fd32bba6f4f00d707cb32de0e8
edilson <94394633+edilsonpx@users.noreply.github.com>
2022-12-14 20:28:07 -0300

    Update configuração.js

--- js/configuração.js
+++ js/configuração.js
@@ -155,7 +155,7 @@ var  firebaseConfig = {
                 <section class="container5">
                 <div class=" w50 left box-central">
                         <div class="card-curso4">
-                          <img  class="foto" src ="${val.data().arquivoURL}"
+                          <img  class="foto" src ="${val.data().arquivoURL}"</div>
                            <p class="descriçao">${val.data().descricao}</p>
                             </div>
                             </div>
